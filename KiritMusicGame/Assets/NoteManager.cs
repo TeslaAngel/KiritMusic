8b68ed544e44b24576361b45ccdbfe2dacf97e85
TeslaAngel <42201165+TeslaAngel@users.noreply.github.com>
2020-12-27 23:18:54 +0800

    Initing plugin

---
+++ KiritMusicGame/Assets/NoteManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class NoteManager : MonoBehaviour
+{
+
+    //public float Tempo;
+    //public float Divide1BeatInto;
+    public Koreography koreography;
+
+    public GameObject NoteBeat;
+    public string eventID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Koreographer.Instance.RegisterForEvents(eventID, NoteEvent);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void NoteEvent(KoreographyEvent koreographyEvent)
+    {
+        //todo
+        print("cubess");
+        //Destroy(gameObject);
+    }
+}
---
+++ KiritMusicGame/Assets/TextSong1/TextSong1Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class TextSong1Script : MonoBehaviour
+{
+    public string eventID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Koreographer.Instance.RegisterForEvents(eventID, cubess);
+        print("ready");
+    }
+
+    private void cubess(KoreographyEvent koreographyEvent)
+    {
+        //todo
+        print("cubess");
+        //Destroy(gameObject);
+    }
+}

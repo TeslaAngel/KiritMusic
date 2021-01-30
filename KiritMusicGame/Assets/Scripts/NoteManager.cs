35c212305d26b9cd0e49cb590dba839e0a135cbb
TeslaAngel <42201165+TeslaAngel@users.noreply.github.com>
2021-01-30 10:02:39 +0800

    Initing Music system

---
+++ KiritMusicGame/Assets/Scripts/NoteManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class NoteManager : MonoBehaviour
+{
+
+    //public float Tempo;
+    //public float Divide1BeatInto;
+    //public Koreography koreography;
+
+    public GameObject NoteBeat;
+    public string NoteBeatEventID;
+
+    [Space]
+    public Transform[] Tracks;
+    //public List<GameObject> Notes = new List<GameObject>();
+    public GameObject[] NoteBeats;
+
+    public float NoteSpeed;
+    public float EndVertical;
+    public float NoteDiameter;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Koreographer.Instance.RegisterForEvents(NoteBeatEventID, NoteBeatEvent);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int I=0; I< NoteBeats.Length; I++)
+        {
+            NoteBeats[I].transform.Translate(0, 0, -NoteSpeed * Time.deltaTime);
+
+            if (NoteBeats[I].transform.position.z < EndVertical - NoteDiameter)
+            {
+                Destroy(NoteBeats[I]);
+                NoteBeats = GameObject.FindGameObjectsWithTag("BeatNote");
+                
+                
+            }
+
+        }
+    }
+
+    //NoteBeat Instantiate
+    private void NoteBeatEvent(KoreographyEvent koreographyEvent)
+    {
+        
+        //todo
+        print("NoteBeat");
+        //Destroy(gameObject);
+
+        int Track = koreographyEvent.GetIntValue();
+        Track -= 1;
+        Instantiate(NoteBeat, Tracks[Track].transform.position, Tracks[Track].transform.rotation, Tracks[Track].transform);
+        //Notes.Add(Tracks[Track].transform.Find("NoteBeat").gameObject);
+        //Notes[Notes.Count - 1].transform.SetParent(null);
+        NoteBeats = GameObject.FindGameObjectsWithTag("BeatNote");
+    }
+}
---
+++ KiritMusicGame/Assets/Scripts/RelayMusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelayMusicPlayer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float RelayTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (RelayTime > 0)
+        {
+            RelayTime -= 1 * Time.deltaTime;
+        }
+        else
+        {
+            audioSource.Play();
+            Destroy(GetComponent<RelayMusicPlayer>());
+        }
+    }
+}
